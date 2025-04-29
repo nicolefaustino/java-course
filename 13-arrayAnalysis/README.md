ad4bf879b500cfdfc7a7285f66f9fa1d1493240b
Nicole Faustino <nicoleadifaustino@gmail.com>
2025-04-29 11:20:52 +0800

    Array Analysis Program

---
+++ 13-arrayAnalysis/README.md
@@ -0,0 +1,3 @@
+# Java Array Analysis Program
+
+This Java program analyzes an array of integers by counting how many elements are greater than the first element in the array.
---
+++ 13-arrayAnalysis/arrayAnalysis.java
@@ -0,0 +1,31 @@
+package Laboratory;
+import java.util.Scanner;
+
+public class Laboratory1 {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in); 
+		System.out.println("What is the size of your array? ");
+		
+		int size = scanner.nextInt();		
+		int[] array = new int[size];
+		System.out.println("Enter " + size + " integers: ");
+		
+		for (int i = 0; i < size; i++){
+			array[i] = scanner.nextInt();
+		}
+		scanner.close();
+	
+		if (array.length > 0) {
+			int firstElement = array[0];
+			int count = 0;
+			for (int i = 1; i < array.length; i++) {
+				if (array[i] > firstElement) {
+					count++;
+		}
+	}
+			System.out.println("Number of integers greater than the first integer: " + count);
+}
+		
+	}
+}
